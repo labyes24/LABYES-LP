da884f14fe6a5bebb0409c52d853e20c954752eb
Evandro <dam450@duck.com>
2024-12-12 15:52:52 -0300

    chore: add route folders structure
    Refs #868b4vn15

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function ContactsPage() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Contato</h1>
+      <Image src="/vercel.svg" alt="Vercel Logo" width={100} height={24} />
+    </>
+  );
+}
