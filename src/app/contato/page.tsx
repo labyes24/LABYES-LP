d6d8da62a7d948db11e7474846ea9c3ab5df1bd6
franciscoylderlan <ylderlan@nca.ufma.br>
2024-12-12 17:09:36 -0300

    style: update file formatting with prettier and tailwindcss sorting
    Refs: 868b93znb

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function ContactsPage() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Contato</h1>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={100} height={24} />
-    </>
-  );
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Contato</h1>
+            <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                width={100}
+                height={24}
+            />
+        </>
+    )
 }
