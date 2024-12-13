d6d7affe80ccb0c6df8f55cc495ac2902bddea12
franciscoylderlan <ylderlan@nca.ufma.br>
2024-12-13 11:21:24 -0300

    style: add color palette
    Refs: 868banf46

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,26 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        "ly-black": "#222222",
+        "ly-white": "#FDFDFD",
+
+        "ly-orange-400": "#FF9500",
+        "ly-orange-500": "#EB8B2C",
+
+        "ly-brown": "#B07D54",
+        "ly-red": "#AA3B2B",
+
+        "ly-green-400": "#02B659",
+        "ly-green-500": "#1DA856",
+
+        "ly-dark-azure-600": "#33475B",
+        "ly-dark-azure-700": "#1E2536",
+        "ly-dark-azure-800": "#1B202A",
+        "ly-dark-azure-transparent": "#33475BE6",
+
+        "ly-gray-50": "#F2F1FA",
+        "ly-gray-100": "#EAF0F6",
       },
     },
   },
