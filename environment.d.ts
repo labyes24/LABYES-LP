74f74175c1254ba7b5dde6a3a830bd196509a585
franciscoylderlan <ylderlan@nca.ufma.br>
2024-12-21 11:18:07 -0300

    fix: add export to environment.d.ts and include it in tsconfig.json
    Refs: 868bet8tn

--- environment.d.ts
+++ environment.d.ts
@@ -1,5 +1,5 @@
 namespace NodeJS {
-    interface ProcessEnv {
+    export interface ProcessEnv {
         NEXT_PUBLIC_CLIENT_LINK_EMAIL: string
         NEXT_PUBLIC_CLIENT_LINK_LINKEDIN: string
         NEXT_PUBLIC_CLIENT_LINK_YOUTUBE: string
