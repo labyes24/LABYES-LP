a09a035902baac9751e59b0f85bf3144bbf29377
Evandro <dam450@duck.com>
2025-01-07 13:14:50 -0300

    feat: create projects page

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,18 +1,25 @@
 import { Metadata } from 'next'
-import Image from 'next/image'
+
+import BottomRight from '@/assets/projects_bottom_mobo.svg'
+import TopLeft from '@/assets/projects_top_mobo.svg'
+
 export const metadata: Metadata = {
     title: 'Projetos',
 }
 export default function ProjectsPage() {
     return (
-        <>
-            <h1 className="text-3xl font-bold underline">Projetos</h1>
-            <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                width={100}
-                height={24}
+        <section className="relative flex min-h-[80vh] flex-col items-center overflow-hidden pt-16 text-ly-dark-azure-600">
+            <img src={TopLeft.src} className="absolute left-0 top-0 -z-10" />
+            <img
+                src={BottomRight.src}
+                className="absolute bottom-0 right-0 -z-10"
             />
-        </>
+
+            <h2 className="text-3xl font-bold">Projetos</h2>
+            <p className="mt-4 px-4 text-center leading-7">
+                Lorem ipsum dolor sit amet consectetur adipiscing elit semper
+                dalar elementum tempus hac tellus libero accumsan.
+            </p>
+        </section>
     )
 }
