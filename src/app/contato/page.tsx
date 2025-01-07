b8bdad8aa3faa408721b4451660e80499d4db6be
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2025-01-07 19:10:00 -0300

    style: add contact page structure
    Refs: 868bd1wuy

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,18 +1,78 @@
 import { Metadata } from 'next'
-import Image from 'next/image'
+import Form from 'next/form'
 export const metadata: Metadata = {
     title: 'Contatos',
 }
 export default function ContactsPage() {
     return (
-        <>
-            <h1 className="text-3xl font-bold underline">Contato</h1>
-            <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                width={100}
-                height={24}
-            />
-        </>
+        <div className="content-wrapper">
+            <section className="section-wrapper">
+                <div className="section-info">
+                    <h2>Titulo da Sessão</h2>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit.
+                    </p>
+                </div>
+                <div className="empresa-info">
+                    <span></span>
+                    <strong>Empresa</strong>
+                </div>
+                <div className="talentos-info">
+                    <span></span>
+                    <strong>Talentos</strong>
+                </div>
+            </section>
+            <Form action="/">
+                <div className="buttons-wrapper">
+                    <button>Empresa</button>
+                    <button>Dev junior</button>
+                </div>
+                <h3>Entre em contato</h3>
+                <p>Queremos ouvir você</p>
+
+                <div>
+                    <label htmlFor="name"></label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Nome"
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email"></label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="E-mail"
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="message"></label>
+                    <textarea
+                        id="message"
+                        name="message"
+                        placeholder="Digite sua mensagem"
+                        required
+                    ></textarea>
+                </div>
+                <button type="submit">Enviar mensagem</button>
+            </Form>
+            <section className="contact-info">
+                <div className="adress">
+                    <h2>Endereço</h2>
+                    <p>58 Middle Point Rd San Francisco, 94124</p>
+                </div>
+                <div className="contact">
+                    <h2>Contatos</h2>
+                    <p>(12)3456-7890</p>
+                    <p>contato@email.com</p>
+                </div>
+            </section>
+        </div>
     )
 }
