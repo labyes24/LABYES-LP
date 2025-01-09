c4d53163e56c9ad4a9bff7442d3aab624e7c0e14
Evandro <dam450@duck.com>
2025-01-09 16:20:10 -0300

    feat: create supporters section in home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,20 @@
-import Image from 'next/image'
+import Doodle from '@/assets/supporters_doodle_full.svg'
 
 export default function Home() {
     return (
         <>
             <h1 className="text-3xl font-bold underline">Home</h1>
-            <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                width={100}
-                height={24}
-            />
+
+            <section
+                className="flex min-h-[50vh] flex-col items-center pb-[9rem] pt-[5rem] font-['DM_Sans'] text-ly-dark-azure-600"
+                style={{
+                    backgroundImage: `url(${Doodle.src})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'bottom -185px right -220px',
+                }}
+            >
+                <h2 className="text-3xl font-bold">Apoiadores</h2>
+            </section>
         </>
     )
 }
