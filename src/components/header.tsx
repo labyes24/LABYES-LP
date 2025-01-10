27958cc8560b93b62cc61c886fa8f9ef803f0089
franciscoylderlan <ylderlan@nca.ufma.br>
2025-01-10 10:40:25 -0300

    fix: remove excessive padding/margin and fix z-index of header layer
    Refs: 868bubh3b

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import { LogoLabYes } from './svg/logo-lab-yes'
 
 export function Header() {
     return (
-        <header className="sticky left-0 right-0 top-0 mb-6 flex h-fit items-center justify-between gap-6 border-b border-muted bg-ly-white p-6 md:px-36 md:py-8">
+        <header className="sticky left-0 right-0 top-0 z-50 flex h-fit items-center justify-between gap-6 border-b border-muted bg-ly-white p-6 md:px-36 md:py-8">
             <Link
                 href={'/'}
                 className="inline-flex w-fit select-none p-0 hover:cursor-pointer"
