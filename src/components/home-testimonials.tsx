a12425cadef4bb8dea36683f77c812b79928886f
franciscoylderlan <ylderlan@nca.ufma.br>
2025-01-11 08:50:03 -0300

    feat: add background to testimonials section
    Refs: 868bd1qqd

--- src/components/home-testimonials.tsx
+++ src/components/home-testimonials.tsx
@@ -1,13 +1,24 @@
+import backgroundImage from '@/assets/home-testimonials-background.webp'
+import Image from 'next/image'
 import { DoodleUnderline } from './svg/doodle-underline'
 import { TestimonialCard } from './testimonial-card'
-
 export function HomeTestimonials() {
     return (
-        <section className="flex w-full flex-col gap-6 border-4 border-ly-red lg:px-24 lg:py-11">
-            <div className="mx-auto max-w-[630px] text-center">
+        <section className="relative flex w-full flex-col gap-10 px-7 py-14 lg:px-24 lg:py-28">
+            <div className="absolute bottom-0 right-0 -z-10 size-full bg-ly-dark-azure-transparent fill-inherit" />
+            <Image
+                src={backgroundImage}
+                priority={true}
+                unoptimized
+                alt="Fundo de profissional da programação programando."
+                fill={true}
+                sizes="100%"
+                className="-z-20 rounded-lg object-cover"
+            />
+            <div className="mx-auto flex max-w-[630px] flex-col items-center justify-center gap-4 text-center text-ly-white">
                 <h2 className="flex flex-col pb-4 text-2xl font-bold lg:text-3xl">
                     Depoimentos
-                    <DoodleUnderline color="default" size="thin" />
+                    <DoodleUnderline color="red" size="thin" />
                 </h2>
                 <p className="text-pretty leading-7 tracking-widest">
                     Lorem ipsum dolor sit amet consectetur adipiscing elit
@@ -16,25 +27,28 @@ export function HomeTestimonials() {
             </div>
             <div className="flex flex-col gap-6 lg:flex-row">
                 <TestimonialCard
-                    quote=""
-                    description=""
-                    author=""
-                    role=""
-                    company=""
+                    positionCard={0}
+                    quote="“An amazing service”"
+                    description="Lorem ipsum dolor sit ametolil col consectetur adipiscing lectus a nunc mauris scelerisque sed egestas."
+                    author="John Carter"
+                    role="Designer"
+                    company="BRIX Templates"
                 />
                 <TestimonialCard
-                    quote=""
-                    description=""
-                    author=""
-                    role=""
-                    company=""
+                    positionCard={1}
+                    quote="“One of a kind service”"
+                    description="Ultrices eros in cursus turpis massa tincidunt sem nulla pharetra diam sit amet nisl suscipit adipis."
+                    author="Sophie Moore"
+                    role="Head of Design"
+                    company="BRIX Templates"
                 />
                 <TestimonialCard
-                    quote=""
-                    description=""
-                    author=""
-                    role=""
-                    company=""
+                    positionCard={2}
+                    quote="“The best service”"
+                    description="Convallis posuere morbi leo urna molestie at elementum eu facilisis sapien pellentesque habitant."
+                    author="Andy Smith"
+                    role="Developer"
+                    company="BRIX Templates"
                 />
             </div>
         </section>
