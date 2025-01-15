e2ccf52e9f6825310828706d88afc4f3f0e95969
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2025-01-15 16:27:06 -0300

    feat: create culture session in About Us page
    Refs: 868bd1vfz

--- src/app/quemsomos/page.tsx
+++ src/app/quemsomos/page.tsx
@@ -1,18 +1,13 @@
 import { Metadata } from 'next'
-import Image from 'next/image'
+
+import { AboutCulture } from '@/components/about-culture'
 export const metadata: Metadata = {
     title: 'Quem somos',
 }
-export default function WhoUsPage() {
+export default function AboutUsPage() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Quem somos</h1>
-            <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                width={100}
-                height={24}
-            />
+            <AboutCulture />
         </>
     )
 }
