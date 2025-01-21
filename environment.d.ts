5aeef053384103138ec9576b0af7da8ef6947603
franciscoylderlan <ylderlan@nca.ufma.br>
2025-01-21 11:44:50 -0300

    chore: add environment variables for Nodemailer
    Refs: 868bd1wxx

--- environment.d.ts
+++ environment.d.ts
@@ -3,5 +3,9 @@ namespace NodeJS {
         NEXT_PUBLIC_CLIENT_LINK_EMAIL: string
         NEXT_PUBLIC_CLIENT_LINK_LINKEDIN: string
         NEXT_PUBLIC_CLIENT_LINK_YOUTUBE: string
+
+        SEND_EMAIL_PROTOCOL: string
+        APPLICATION_EMAIL: string
+        APPLICATION_EMAIL_PASSWORD: string
     }
 }
