6abf349788a01b2317b8dedd8534460cf3f9f695
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2025-01-21 10:54:50 -0300

    refactor: add about manifest Item to components folder
    Refs: 868bd1vmz

--- src/components/about-manifest.tsx
+++ src/components/about-manifest.tsx
@@ -1,10 +1,8 @@
-import Image from 'next/image'
-
-import PlaceholderImage from '@/assets/project_card_placeholder.svg'
+import { AboutManifestItem } from '@/components/about-manifest-item'
 
 export function AboutManifest() {
     return (
-        <section className="flex w-full flex-col items-center justify-center gap-10 p-4">
+        <section className="mb-16 flex w-full flex-col items-center justify-center gap-9 p-4">
             <div className="mx-auto flex max-w-[430px] flex-col items-center justify-center text-center text-ly-dark-azure-600">
                 <strong className="mb-4 mt-14 flex flex-col text-pretty font-sans text-[18px]/[20px] font-bold uppercase tracking-widest text-ly-dark-azure-600">
                     Our process
@@ -13,28 +11,24 @@ export function AboutManifest() {
                     Manifesto
                 </h2>
 
-                <p className="mb-9 text-pretty font-sans text-base/[28px] font-normal text-ly-dark-azure-600">
+                <p className="text-pretty font-sans text-base/[28px] font-normal text-ly-dark-azure-600">
                     Lorem ipsum dolor sit amet consectetur adipiscing eli mattis
                     sit phasellus mollis.
                 </p>
             </div>
-            <div className="mx-auto flex max-w-[430px] flex-col items-center justify-center text-center text-ly-dark-azure-600">
-                <Image
-                    src={PlaceholderImage}
-                    priority={true}
-                    width={80}
-                    height={80}
-                    unoptimized
-                    alt="Imagem de Placeholder"
-                    className="mb-4"
+            <div className="mx-auto mb-6 flex max-w-[430px] flex-col items-center justify-center text-center text-ly-dark-azure-600">
+                <AboutManifestItem
+                    title="Download our free app"
+                    description="Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalar consectur elementum tempus hac."
                 />
-                <h3 className="mb-3 font-sans text-lg/6 font-bold text-ly-dark-azure-600">
-                    Download our free app
-                </h3>
-                <p className="mb-9 text-pretty font-sans text-base/[28px] font-normal text-ly-dark-azure-600">
-                    Lorem ipsum dolor sit amet consecte tur adipiscing elit
-                    semper dalar consectur elementum tempus hac.
-                </p>
+                <AboutManifestItem
+                    title="Create an account"
+                    description="Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalar consectur elementum tempus hac."
+                />
+                <AboutManifestItem
+                    title="Enjoy our app"
+                    description="Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalar consectur elementum tempus hac."
+                />
             </div>
         </section>
     )
