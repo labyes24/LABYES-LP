e63433c2351d48a81a8d0ffe4f5f634cbe2859e6
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2025-01-21 10:39:54 -0300

    feat: add manifest session styles
    Refs: 868bd1vmz

--- src/app/quemsomos/page.tsx
+++ src/app/quemsomos/page.tsx
@@ -1,18 +1,12 @@
+import { AboutManifest } from '@/components/about-manifest'
 import { Metadata } from 'next'
-import Image from 'next/image'
 export const metadata: Metadata = {
     title: 'Quem somos',
 }
 export default function WhoUsPage() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Quem somos</h1>
-            <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                width={100}
-                height={24}
-            />
+            <AboutManifest />
         </>
     )
 }
