e92352054971dff04f7e8c53d731e7f00e3d1dc1
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2025-01-21 10:39:54 -0300

    feat: add manifest session styles
    Refs: 868bd1vmz

--- src/components/about-manifest.tsx
+++ src/components/about-manifest.tsx
@@ -4,32 +4,34 @@ import PlaceholderImage from '@/assets/project_card_placeholder.svg'
 
 export function AboutManifest() {
     return (
-        <section className="relative flex w-full flex-col gap-10 p-7">
-            <div className="mx-auto flex max-w-[630px] flex-col items-center justify-center text-center text-ly-white">
-                <strong className="mb-2 mt-52 flex flex-col text-pretty font-sans text-[28px]/[38px] font-bold text-ly-white lg:text-3xl">
+        <section className="flex w-full flex-col items-center justify-center gap-10 p-4">
+            <div className="mx-auto flex max-w-[430px] flex-col items-center justify-center text-center text-ly-dark-azure-600">
+                <strong className="mb-4 mt-14 flex flex-col text-pretty font-sans text-[18px]/[20px] font-bold uppercase tracking-widest text-ly-dark-azure-600">
                     Our process
                 </strong>
-                <h2 className="text-pretty font-sans text-lg/7 font-bold">
+                <h2 className="mb-4 text-pretty font-sans text-[28px]/[38px] font-bold">
                     Manifesto
                 </h2>
 
-                <p className="mb-24 text-pretty indent-0.5 font-sans text-[22px]/9 font-normal">
+                <p className="mb-9 text-pretty font-sans text-base/[28px] font-normal text-ly-dark-azure-600">
                     Lorem ipsum dolor sit amet consectetur adipiscing eli mattis
                     sit phasellus mollis.
                 </p>
             </div>
-            <div>
+            <div className="mx-auto flex max-w-[430px] flex-col items-center justify-center text-center text-ly-dark-azure-600">
                 <Image
                     src={PlaceholderImage}
                     priority={true}
+                    width={80}
+                    height={80}
                     unoptimized
                     alt="Imagem de Placeholder"
-                    fill={true}
-                    sizes="100%"
-                    className="-z-20 rounded-lg object-cover"
+                    className="mb-4"
                 />
-                <h3>Download our free app</h3>
-                <p>
+                <h3 className="mb-3 font-sans text-lg/6 font-bold text-ly-dark-azure-600">
+                    Download our free app
+                </h3>
+                <p className="mb-9 text-pretty font-sans text-base/[28px] font-normal text-ly-dark-azure-600">
                     Lorem ipsum dolor sit amet consecte tur adipiscing elit
                     semper dalar consectur elementum tempus hac.
                 </p>
