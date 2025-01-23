12eb509c10bf9746be3e34c48855c0e2ede36ad6
franciscoylderlan <ylderlan@nca.ufma.br>
2025-01-23 15:25:52 -0300

    style: adjust spacing for mobile version
    Refs: 868bd1rqt

--- src/components/about-history.tsx
+++ src/components/about-history.tsx
@@ -2,10 +2,10 @@ import { AboutHistoryTimeline } from './about-history-timeline'
 
 export function AboutHistory() {
     return (
-        <section className="my-16 flex flex-col gap-6 lg:gap-16">
+        <section className="my-16 flex flex-col gap-6 px-6 lg:gap-16">
             <div className="flex flex-col items-center justify-center gap-4 text-center text-ly-dark-azure-600">
                 <h2 className="text-3xl font-medium lg:text-4xl">História</h2>
-                <p className="max-w-md text-pretty leading-7 tracking-widest">
+                <p className="max-w-md text-pretty text-sm leading-7 tracking-widest lg:text-base">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                     do eiusmod tempor incididunt ut labore et
                 </p>
