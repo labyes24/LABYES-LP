7abf694df129aac7632e4229e302ca2f36cfdd25
Evandro <dam450@duck.com>
2025-02-06 19:52:22 -0300

    feat: update layout and positioning in HomeIntro component

--- src/components/home-intro.tsx
+++ src/components/home-intro.tsx
@@ -4,8 +4,9 @@ import { Separator } from './ui/separator'
 
 export function HomeIntro() {
     return (
-        <section className="relative my-10 flex flex-col gap-10">
-            <DoodleHeartArrow className="absolute -top-5 left-0 h-16 w-16 lg:top-0 lg:h-28 lg:w-28" />
+        <section className="relative flex flex-col gap-10 px-6 py-10 pt-20 lg:pt-14">
+            <DoodleHeartArrow className="absolute left-8 top-5 -z-[1] h-16 w-16 lg:left-[77px] lg:top-[19px] lg:h-28 lg:w-28" />
+
             <div className="mx-auto flex max-w-[720px] flex-col gap-4 text-center text-ly-dark-azure-600 lg:gap-6">
                 <h2 className="text-2xl font-bold lg:text-3xl">Introdução</h2>
                 <p className="text-lg font-light leading-loose lg:leading-loose lg:tracking-widest">
@@ -18,11 +19,13 @@ export function HomeIntro() {
                     team.&quot;
                 </p>
             </div>
+
             <div className="flex flex-col">
                 <Separator />
                 <span className="mx-auto w-44 border-b-2 border-ly-dark-azure-600 lg:w-96" />
             </div>
-            <DoodleLight className="absolute -right-2 bottom-0 h-20 w-20 lg:h-36 lg:w-36" />
+
+            <DoodleLight className="absolute bottom-8 right-4 -z-[1] h-20 w-20 lg:h-36 lg:w-36" />
         </section>
     )
 }
