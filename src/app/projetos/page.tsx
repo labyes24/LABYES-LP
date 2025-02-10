1cb674da17b83fff1dedbe5623b8f09b8fd22e5d
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2025-02-10 16:07:31 -0300

    style: adjusted project page   styles for desktop
    Refs: 868c8kb3d

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -18,33 +18,35 @@ export default function ProjectsPage() {
             }}
             className="flex min-h-[80vh] flex-col items-center pt-16 font-['DM_Sans'] text-ly-dark-azure-600"
         >
-            <h2 className="text-3xl font-bold">Projetos</h2>
+            <h2 className="text-3xl font-bold lg:text-4xl/[46px]">Projetos</h2>
 
-            <p className="mb-9 mt-4 px-4 text-center leading-7">
+            <p className="mb-9 mt-4 px-4 text-center leading-7 lg:mb-14 lg:max-w-[620px] lg:p-0 lg:text-lg/[30px]">
                 Lorem ipsum dolor sit amet consectetur adipiscing elit semper
                 dalar elementum tempus hac tellus libero accumsan.
             </p>
 
-            <ProjectCard
-                title="Mobile App"
-                description="Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalar dolor elementum tempus hac."
-                finished={true}
-                link="/projetos"
-            />
+            <div className="mb-24 flex flex-col items-center font-['DM_Sans'] text-ly-dark-azure-600 lg:mb-32 lg:flex-row lg:gap-7">
+                <ProjectCard
+                    title="Mobile App"
+                    description="Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalar dolor elementum tempus hac."
+                    finished={true}
+                    link="/projetos"
+                />
 
-            <ProjectCard
-                title="Desktop App"
-                description="Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalar dolor elementum tempus hac."
-                finished={false}
-                link="/projetos"
-            />
+                <ProjectCard
+                    title="Desktop App"
+                    description="Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalar dolor elementum tempus hac."
+                    finished={false}
+                    link="/projetos"
+                />
 
-            <ProjectCard
-                title="Multiple Users"
-                description="Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalar dolor elementum tempus hac."
-                finished={false}
-                link="/projetos"
-            />
+                <ProjectCard
+                    title="Multiple Users"
+                    description="Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalar dolor elementum tempus hac."
+                    finished={false}
+                    link="/projetos"
+                />
+            </div>
         </section>
     )
 }
