678ac66fb77569b2cd0c319d7bd3d9007a50200b
Evandro <dam450@duck.com>
2025-02-14 17:12:13 -0300

    feat: update environment configuration and add Google Analytics support
    refs: 868c8h01k

--- environment.d.ts
+++ environment.d.ts
@@ -1,5 +1,7 @@
-namespace NodeJS {
-    export interface ProcessEnv {
+// environment.d.ts
+
+declare namespace NodeJS {
+    interface ProcessEnv {
         NEXT_PUBLIC_CLIENT_LINK_EMAIL: string
         NEXT_PUBLIC_CLIENT_LINK_LINKEDIN: string
         NEXT_PUBLIC_CLIENT_LINK_YOUTUBE: string
@@ -7,5 +9,7 @@ namespace NodeJS {
         SEND_EMAIL_PROTOCOL: string
         APPLICATION_EMAIL: string
         APPLICATION_EMAIL_PASSWORD: string
+
+        NEXT_PUBLIC_GOOGLE_ANALYTICS_ID: string | undefined
     }
 }
