126e03e035ee510aa1edfdcb649b74772197de48
Evandro <dam450@duck.com>
2025-02-27 16:45:30 -0300

    style: update AboutHistory component with new background and text colors
    refs: 868ctbhja

--- src/components/about-history.tsx
+++ src/components/about-history.tsx
@@ -2,8 +2,8 @@ import { AboutHistoryTimeline } from './about-history-timeline'
 
 export function AboutHistory() {
     return (
-        <section className="my-16 flex flex-col gap-6 px-6 lg:gap-16">
-            <div className="flex flex-col items-center justify-center gap-4 text-center text-ly-dark-azure-600">
+        <section className="flex flex-col gap-6 bg-section-light px-6 py-16 text-section-light-foreground lg:gap-16">
+            <div className="flex flex-col items-center justify-center gap-4 text-center">
                 <h2 className="text-3xl font-medium lg:text-5xl">História</h2>
                 <p className="max-w-md text-pretty text-sm leading-7 tracking-widest lg:text-lg">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
