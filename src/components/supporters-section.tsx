f9508bc53f58e359c036f91e0aa04ed6faf3a65a
Evandro <dam450@duck.com>
2025-03-06 14:51:03 -0300

    chore: update supporters section to use new SVG logos

--- src/components/supporters-section.tsx
+++ src/components/supporters-section.tsx
@@ -1,16 +1,28 @@
-import Supporter01 from '@/assets/supporter_01.svg'
-import Supporter02 from '@/assets/supporter_02.svg'
-import Supporter03 from '@/assets/supporter_03.svg'
-import Supporter04 from '@/assets/supporter_04.svg'
-import Supporter05 from '@/assets/supporter_05.svg'
-import Supporter06 from '@/assets/supporter_06.svg'
 import Doodle from '@/assets/supporters_doodle.svg'
+import { LogoGilmar } from './svg/logo-gilmar'
+import { LogoRK } from './svg/logo-rk'
+import { LogoSSIG } from './svg/logo-ssig'
+
+const supporters = [
+    {
+        id: 1,
+        SVG: LogoGilmar,
+    },
+    {
+        id: 2,
+        SVG: LogoRK,
+    },
+    {
+        id: 3,
+        SVG: LogoSSIG,
+    },
+]
 
 export function SupportersSection() {
     return (
         <>
             <section
-                className="flex min-h-[50vh] flex-col items-center bg-section-light pb-36 pt-20 font-['DM_Sans'] text-section-light-foreground"
+                className="flex min-h-[50vh] flex-col items-center bg-section-light pb-36 pt-20 font-sans text-section-light-foreground"
                 style={{
                     backgroundImage: `url(${Doodle.src})`,
                     backgroundRepeat: 'no-repeat',
@@ -19,25 +31,21 @@ export function SupportersSection() {
             >
                 <h2 className="mb-28 text-3xl font-bold">Apoiadores</h2>
 
-                <ul className="grid grid-cols-2 justify-center gap-x-16 gap-y-20 px-8 lg:flex">
-                    <li className="flex justify-center">
-                        <img src={Supporter01.src} alt="" />
-                    </li>
-                    <li className="flex justify-center">
-                        <img src={Supporter02.src} alt="" />
-                    </li>
-                    <li className="flex justify-center">
-                        <img src={Supporter03.src} alt="" />
-                    </li>
-                    <li className="flex justify-center">
-                        <img src={Supporter04.src} alt="" />
-                    </li>
-                    <li className="flex justify-center">
-                        <img src={Supporter05.src} alt="" />
-                    </li>
-                    <li className="flex justify-center">
-                        <img src={Supporter06.src} alt="" />
-                    </li>
+                <ul className="grid grid-cols-1 justify-center gap-x-16 gap-y-20 px-8 lg:flex lg:w-full lg:justify-around">
+                    {supporters.map((supporter) => {
+                        const { SVG } = supporter
+                        return (
+                            <li
+                                key={supporter.id}
+                                className="flex justify-center align-middle"
+                            >
+                                <SVG
+                                    fill="hsl(var(--section-light-foreground))"
+                                    className="h-20 lg:h-24"
+                                />
+                            </li>
+                        )
+                    })}
                 </ul>
             </section>
         </>
