eb1382ec50c203b079f6ba85516bc8b5559a6666
luisvinicius403 <luisvinicius.dev@gmail.com>
2025-03-25 15:29:35 -0300

    chore: adjust commit after format

--- src/components/about-history-timeline.tsx
+++ src/components/about-history-timeline.tsx
@@ -1,10 +1,10 @@
 import { Lightbulb, PackageCheck, UsersRound } from 'lucide-react'
+import Link from 'next/link'
 import {
     AboutHistoryTimelineItem,
     AboutHistoryTimelineItemProps,
 } from './about-history-timeline-item'
 import { AboutHistoryLine } from './svg/about-history-line'
-import Link from 'next/link'
 
 export function AboutHistoryTimeline() {
     const timelineData: AboutHistoryTimelineItemProps[] = [
@@ -12,9 +12,10 @@ export function AboutHistoryTimeline() {
             title: 'Ideação',
             description: (
                 <>
-                    Como parte da comunidade, percebemos que o mercado real é fechado para novos devs.
-                    Com isso, imaginamos uma ponte entre os recém-formados e o mercado real,
-                    nascendo assim o <span className='font-bold'>Lab Yes!</span>
+                    Como parte da comunidade, percebemos que o mercado real é
+                    fechado para novos devs. Com isso, imaginamos uma ponte
+                    entre os recém-formados e o mercado real, nascendo assim o{' '}
+                    <span className="font-bold">Lab Yes!</span>
                 </>
             ),
             icon: Lightbulb,
@@ -23,9 +24,11 @@ export function AboutHistoryTimeline() {
             title: 'O início',
             description: (
                 <>
-                    Em março de 2023, com um time pequeno, mas altamente comprometido, superamos
-                    desafios e entregamos projetos de sucessos, conquistando a confiança do cliente e
-                    abrindo novas oportunidades para o <span className='font-bold'>Lab Yes!</span>
+                    Em março de 2023, com um time pequeno, mas altamente
+                    comprometido, superamos desafios e entregamos projetos de
+                    sucessos, conquistando a confiança do cliente e abrindo
+                    novas oportunidades para o{' '}
+                    <span className="font-bold">Lab Yes!</span>
                 </>
             ),
             icon: UsersRound,
@@ -34,10 +37,17 @@ export function AboutHistoryTimeline() {
             title: 'Primeiros Projetos',
             description: (
                 <>
-                    Desenvolvemos nove landing pages, incluindo perfis dos corretores, cards de imóveis
-                    com modal e formulários, atendendo às necessidades específicas 
-                    da <Link href="https://www.rkimoveis.com.br/" className='font-bold'>RK Imóveis.</Link> A 
-                    empresa nos impulsionou fortemente permitindo-nos entregar soluções personalizadas e eficazes.
+                    Desenvolvemos nove landing pages, incluindo perfis dos
+                    corretores, cards de imóveis com modal e formulários,
+                    atendendo às necessidades específicas da{' '}
+                    <Link
+                        href="https://www.rkimoveis.com.br/"
+                        className="font-bold"
+                    >
+                        RK Imóveis.
+                    </Link>{' '}
+                    A empresa nos impulsionou fortemente permitindo-nos entregar
+                    soluções personalizadas e eficazes.
                 </>
             ),
             icon: PackageCheck,
