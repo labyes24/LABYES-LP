740ee3506009afa89cfdbd45a14592dee7e8b2c5
Evandro <dam450@duck.com>
2025-03-26 10:43:23 -0300

    fix: update section titles and descriptions in ConnectionsLastProduction component
    change "projetos" to "produtos"
    
    refs: 868d65r3z

--- src/components/connections-last-productions.tsx
+++ src/components/connections-last-productions.tsx
@@ -32,12 +32,15 @@ export function ConnectionsLastProduction() {
         <section className="flex w-full flex-col items-center justify-center gap-12 bg-section-medium px-6 pb-[75px] pt-16 md:pt-32">
             <div className="mx-auto flex max-w-[430px] flex-col items-center justify-center text-center text-section-medium-foreground md:max-w-[614px]">
                 <h2 className="mb-4 text-pretty font-sans text-[28px]/[38px] font-bold md:text-4xl md:leading-tight">
-                    Projetos em destaque
+                    Produtos em destaque
                 </h2>
 
+                <p className="text-pretty px-4 font-sans text-base font-normal leading-7 text-section-medium-foreground md:text-lg">
+                    Aqui estão alguns dos produtos desenvolvidos pelo time{' '}
+                    <span className="font-bold">Lab Yes!</span>.
+                </p>
                 <p className="text-pretty font-sans text-base font-normal leading-7 text-section-medium-foreground md:text-lg">
-                    Aqui estão alguns dos projetos desenvolvidos pelo time Lab
-                    Yes!. Não deixe de conferir.
+                    Não deixe de conferir.
                 </p>
             </div>
 
@@ -58,7 +61,7 @@ export function ConnectionsLastProduction() {
                 className="rounded-full bg-ly-orange-400 px-6 py-3 text-center font-sans text-lg font-bold leading-relaxed text-ly-white hover:bg-ly-orange-500"
                 size={'lg'}
             >
-                <Link href={'/projetos'}>Saiba mais</Link>
+                <Link href={'/produtos'}>Saiba mais</Link>
             </Button>
         </section>
     )
