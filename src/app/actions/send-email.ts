0b516a53318fdfed71bc3567b5e98e9314f90861
Valdemir Filho <filhux@gmail.com>
2025-04-23 17:38:27 -0300

    remove unused import and comments

--- src/app/actions/send-email.ts
+++ src/app/actions/send-email.ts
@@ -6,7 +6,6 @@ import {
     NOTIFICATION_TEAM_EMAIL_TEMPLATE_PATH,
     sendNotificationEmail,
 } from '@/lib/node-mailer'
-// import { generateRandomToken } from '@/lib/utils'
 
 const userTypeMap = {
     enterprise: 'Empresa',
@@ -24,8 +23,6 @@ export async function sendEmail(
 
     const userType = userTypeMap[userTypeKey] || ''
 
-    // const randomToken = generateRandomToken()
-
     const subjectTeam = 'Nova mensagem recebida no Lab Yes!'
     const subjectUser = 'Oba! Sua mensagem chegou com sucesso. 😊'
 
@@ -41,8 +38,6 @@ export async function sendEmail(
             subject: subjectTeam,
             templatePath: NOTIFICATION_TEAM_EMAIL_TEMPLATE_PATH,
             toEmail: process.env.APPLICATION_EMAIL,
-            // Comente a linha acima e descomente a linha abaixo para testar com Mailtrap.
-            // toEmail: email,
         })
 
         await sendNotificationEmail({
