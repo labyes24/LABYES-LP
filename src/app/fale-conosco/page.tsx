55abbd58800a8388ef0c869f4c109115b6c785d3
Evandro <dam450@duck.com>
2026-05-26 22:25:48 -0300

    fix: align Contact Us hero spacing and typography with the proposed design
    refs: LBYS-43

--- src/app/fale-conosco/page.tsx
+++ src/app/fale-conosco/page.tsx
@@ -9,11 +9,11 @@ export const metadata: Metadata = {
 export default function ContactUsPage() {
     return (
         <>
-            <section className="m-auto flex flex-col items-center gap-[15px] bg-[url('/img/scribble.svg')] bg-cover bg-center px-4 pb-[78.94px] pt-[78.76px] sm:pb-[105px] sm:pt-[90px] lg:pb-[126px] lg:pt-[76.36px]">
+            <section className="m-auto flex flex-col items-center gap-[15px] bg-[url('/img/scribble.svg')] bg-cover bg-center px-4 pb-[78.94px] pt-[78.76px] sm:pb-[102px] sm:pt-[102px] lg:pb-[126px] lg:pt-[76.32px]">
                 <h1 className="text-center text-[1.645rem] font-bold leading-none text-primary sm:text-[2.025rem] lg:text-[2.25rem]">
                     #boraláserfeliz!
                 </h1>
-                <div className="flex max-w-[374px] flex-col gap-1.5 text-center text-[0.8225rem] font-bold leading-[1.175rem] sm:max-w-[480px] lg:max-w-[40.625rem]">
+                <div className="flex max-w-[374px] flex-col gap-1.5 text-center text-[0.8225rem] font-bold leading-[1.175rem] sm:max-w-[480px] lg:max-w-[681px] lg:text-[0.84375rem]">
                     <p>
                         Do primeiro passo à maestria corporativa. O Lab Yes! não
                         é um curso, é onde você joga o jogo real atuando em
