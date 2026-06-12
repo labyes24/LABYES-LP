1e1bfd6529164a9822c9fb5eff25d02a08a60a71
Evandro <dam450@duck.com>
2026-06-12 20:31:05 -0300

    fix: adjusted font size and text spacing for various screen sizes
    change text size and spacing in ContactUsHeroSection for better readability
    across devices to accommodate changes in requirements and design (ticket LBYS-57)
    
    ref: LBYS-60

--- src/app/fale-conosco/sections/contact-us-hero.tsx
+++ src/app/fale-conosco/sections/contact-us-hero.tsx
@@ -1,10 +1,10 @@
 export function ContactUsHeroSection() {
     return (
-        <section className="m-auto flex flex-col items-center gap-[15px] bg-[url('/img/scribble.svg')] bg-cover bg-center px-6 pb-[78.94px] pt-[78.76px] sm:pb-[102px] sm:pt-[102px] lg:pb-[126px] lg:pt-[76.32px]">
-            <h1 className="text-center text-[1.645rem] font-bold leading-none text-primary sm:text-[2.025rem] lg:text-[2.25rem]">
+        <section className="m-auto flex min-h-[339px] flex-col items-center gap-[15px] bg-[url('/img/scribble.svg')] bg-cover bg-center px-6 pb-[79px] pt-[79px] sm:gap-[21px] sm:pb-[102px] sm:pt-[102px] lg:gap-[24px] lg:pb-[82px] lg:pt-[72px]">
+            <h1 className="text-center text-3xl font-bold leading-none text-primary sm:text-4xl sm:text-[2.025rem] lg:text-5xl">
                 #boraláserfeliz!
             </h1>
-            <div className="flex max-w-[374px] flex-col gap-1.5 text-center text-[0.8225rem] font-bold leading-[1.175rem] sm:max-w-[480px] lg:max-w-[681px] lg:text-[0.84375rem]">
+            <div className="flex max-w-[374px] flex-col gap-4 text-center text-sm font-bold leading-[1.285] sm:max-w-[480px] sm:text-lg sm:leading-[1.111] lg:max-w-[788px]">
                 <p>
                     Do primeiro passo à maestria corporativa. O Lab Yes! não é
                     um curso, é onde você joga o jogo real atuando em projetos,
