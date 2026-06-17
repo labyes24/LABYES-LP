2ab7ef12e838fcadea6918ee6a50cd98c1200f9c
Evandro <dam450@duck.com>
2026-06-17 18:42:07 -0300

    fix: update Input component styles for improved layout and accessibility

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,3 +1,4 @@
+import { OctagonX } from 'lucide-react'
 import * as React from 'react'
 
 import { cn } from '@/lib/utils'
@@ -5,15 +6,21 @@ import { cn } from '@/lib/utils'
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
     ({ className, type, ...props }, ref) => {
         return (
-            <input
-                type={type}
-                className={cn(
-                    'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-                    className
-                )}
-                ref={ref}
-                {...props}
-            />
+            <div className="relative">
+                <input
+                    type={type}
+                    className={cn(
+                        'peer h-8 w-full min-w-0 rounded-lg border border-input bg-transparent px-4 py-2 text-base outline-none transition-colors file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-ly-dark-azure-600/70 invalid:border-destructive invalid:ring-1 invalid:ring-[#FF5745] focus-visible:border-ring focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-ly-white/50 disabled:opacity-50 aria-[invalid=true]:border-destructive aria-[invalid=true]:ring-1 aria-[invalid=true]:ring-[#FF5745] sm:h-9 md:text-sm',
+                        className
+                    )}
+                    ref={ref}
+                    {...props}
+                />
+                <OctagonX
+                    size={16}
+                    className="absolute right-4 top-1/2 size-4 -translate-y-1/2 text-red-500 opacity-0 transition-opacity duration-200 peer-invalid:opacity-100 peer-aria-[invalid=true]:opacity-100"
+                />
+            </div>
         )
     }
 )
