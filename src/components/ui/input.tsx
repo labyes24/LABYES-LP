2c72a8bf6699cec1940ebc2ee162161ebf9332ba
Evandro <dam450@duck.com>
2026-06-22 00:53:10 -0300

    chore: update input component styles to conform with design

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
                 <input
                     type={type}
                     className={cn(
-                        'peer h-8 w-full min-w-0 rounded-lg border border-input bg-transparent px-4 py-2 text-base outline-none transition-colors file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-ly-dark-azure-600/70 invalid:border-destructive invalid:ring-1 invalid:ring-[#FF5745] focus-visible:border-ring focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-ly-white/50 disabled:opacity-50 aria-[invalid=true]:border-destructive aria-[invalid=true]:ring-1 aria-[invalid=true]:ring-[#FF5745] sm:h-9 md:text-sm',
+                        'peer h-8 w-full min-w-0 rounded-lg border border-input bg-ly-white px-4 py-2 text-base text-ly-dark-azure-850 outline-none transition-colors file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:italic placeholder:text-ly-dark-azure-600/70 invalid:border-destructive invalid:ring-1 invalid:ring-destructive focus-visible:border-ring focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-ly-white/50 disabled:opacity-50 aria-[invalid=true]:border-destructive aria-[invalid=true]:ring-1 aria-[invalid=true]:ring-destructive aria-[invalid=true]:focus-visible:ring-2 sm:h-9 md:text-sm',
                         className
                     )}
                     ref={ref}
@@ -18,7 +18,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
                 />
                 <OctagonX
                     size={16}
-                    className="absolute right-4 top-1/2 size-4 -translate-y-1/2 text-red-500 opacity-0 transition-opacity duration-200 peer-invalid:opacity-100 peer-aria-[invalid=true]:opacity-100"
+                    className="absolute right-4 top-1/2 size-4 -translate-y-1/2 text-destructive opacity-0 transition-opacity duration-200 peer-invalid:opacity-100 peer-aria-[invalid=true]:opacity-100"
                 />
             </div>
         )
