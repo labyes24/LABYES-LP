2645fa07da14653b94c8e5bb6c1bdcc441fe2ddd
Evandro <dam450@duck.com>
2026-06-22 00:50:50 -0300

    chore: adjust text size of InputError component

--- src/components/ui/input-error.tsx
+++ src/components/ui/input-error.tsx
@@ -12,7 +12,7 @@ const InputError = React.forwardRef<
                 <span
                     role="alert"
                     className={cn(
-                        'w-full p-0 invalid:ring-destructive focus-visible:border-ring focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-ly-white/50 disabled:opacity-50 md:text-sm',
+                        'w-full p-0 text-sm invalid:ring-destructive focus-visible:border-ring focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-ly-white/50 disabled:opacity-50 md:text-sm',
                         className
                     )}
                     ref={ref}
