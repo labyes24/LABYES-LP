204d00ef10993515d7d07f3308a6f7196a0b5f52
Evandro <dam450@duck.com>
2026-06-26 16:46:01 -0300

    refactor: update textarea component styles for improved accessibility

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -11,15 +11,16 @@ const Textarea = React.forwardRef<
         <div className="relative">
             <textarea
                 className={cn(
-                    'field-sizing-content aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 peer flex min-h-16 w-full rounded-lg border border-input bg-ly-white p-6 px-2.5 py-2 text-base text-ly-dark-azure-850 outline-none transition-colors placeholder:italic placeholder:text-ly-dark-azure-600/70 focus-visible:border-ring focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-[invalid=true]:border-destructive aria-[invalid=true]:ring-1 aria-[invalid=true]:ring-destructive aria-[invalid=true]:focus-visible:ring-2 sm:h-9 md:text-sm',
+                    'field-sizing-content peer flex min-h-16 w-full rounded-lg border border-input bg-ly-white px-4 py-2 text-sm leading-[18px] text-ly-dark-azure-850 outline-none transition-colors placeholder:text-ly-dark-azure-600/70 focus-visible:border-ring focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-[invalid=true]:border-[#AA3B2B] aria-[invalid=true]:pr-11 aria-[invalid=true]:ring-1 aria-[invalid=true]:ring-[#AA3B2B] aria-[invalid=true]:focus-visible:ring-2 sm:h-9',
                     className
                 )}
                 ref={ref}
                 {...props}
             />
             <OctagonX
+                aria-hidden="true"
                 size={16}
-                className="absolute right-6 top-6 size-4 text-destructive opacity-0 transition-opacity duration-200 peer-invalid:opacity-100 peer-aria-[invalid=true]:opacity-100"
+                className="absolute right-6 top-6 size-4 text-[#AA3B2B] opacity-0 transition-opacity duration-200 peer-invalid:opacity-100 peer-aria-[invalid=true]:opacity-100"
             />
         </div>
     )
